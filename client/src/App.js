5002f9f8d19b8168fbfc8e0d3a439aca0138a9c3
johndavis <johndavis92790@gmail.com>
2022-03-27 13:33:46 -0600

    init

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import API from "./utils/API";
+
+function App() {
+  const apifun = async () => {
+    try {
+      const response = await API.getUsers();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={apifun}></button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    trim: true,
+    allowNull: false,
+  },
+  firstName: {
+    type: String,
+    trim: true,
+    allowNull: false,
+  },
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
