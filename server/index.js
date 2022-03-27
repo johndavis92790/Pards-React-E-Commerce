100fe3ffda55315452f17ebba59b98b73149d862
johndavis <johndavis92790@gmail.com>
2022-03-27 14:01:35 -0600

    fixed server/index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const routes = require("./routes");
+const router = require("express").Router();
 
 const db = require("./config/connection");
 
